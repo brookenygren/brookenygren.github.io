f8d18fa1e271450597002fc7ffdc1976d9b956d9
Brooke Nygren <brooke@brookenygren.com>
2026-07-14 01:06:55 +0000

    fixing a bug where css doesn't deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,10 @@ import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://brookenygren.github.io',
-  base: '/brookenygren.github.io', 
+  base: '/',
+  build: { 
+    assetsInlineLimit: 100000000 
+  },
 
   integrations: [icon()],
 
